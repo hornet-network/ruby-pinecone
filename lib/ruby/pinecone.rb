1f0e0e270571970de278fb409dcb6e84e37745a2
Matthew Hirst <hirst.mat@gmail.com>
2023-02-17 12:57:53 -0300

    Initial commit

---
+++ lib/ruby/pinecone.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "pinecone/version"
+
+module Ruby
+  module Pinecone
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
